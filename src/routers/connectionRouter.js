ff0273248634ebca08da222e141f0fbc554013a7
Diwakar Giri <diwakargiri23@gmail.com>
2025-02-27 15:03:19 +0530

    created profile route like view, edit and connection Route like send request, accept or reject request

--- src/routers/connectionRouter.js
+++ src/routers/connectionRouter.js
@@ -1,8 +1,91 @@
 const express = require('express');
 const User = require('../models/user.model');
 const userAuth = require('../middlewares/userAuth.middleware');
+const ConnectionRequest = require('../models/connectionRequest.model')
 
 const connectionRouter = express.Router();
+
+connectionRouter.post('send/:status/:toUserId', userAuth, async (req, res) => {
+    try {
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        //if Status is allowed or not
+        const allowedStatus = ["interested", "ignored"].includes(status);
+        if (!allowedStatus) {
+            throw new Error("Invalid Status", status);
+        }
+
+        //If user sends request to itself
+        if (fromUserId.toString() === toUserId.toString()) {
+            throw new Error("You cannot send Connection Request ot Yourself!");
+        }
+
+        //If User existed in the DB or not
+        const user = await User.findById(toUserId);
+        if (!user) {
+            throw new Error("Invalid toUserId");
+        }
+        //Connection Already existed or not
+        const existingConnection = await ConnectionRequest.findOne({
+            $or: [
+                { fromUserId: fromUserId, toUserId: toUserId },
+                { fromUserId: toUserId, toUserId: fromUserId }
+            ]
+        });
+
+        if (existingConnection) {
+            throw new Error("Connection Already Existed");
+        }
+
+        const newConnection = await new ConnectionRequest({
+            toUserId,
+            fromUserId,
+            status
+        });
+
+        await newConnection.save();
+
+        res.status(200).json({ message: "Connection Created Successfully", connection: newConnection })
+
+    } catch (error) {
+        res.status(500).json({ message: "Error", error: error.message });
+    }
+})
+
+connectionRouter.post('/review/:status/:fromUserId', userAuth, async (req, res) => {
+    try {
+        const toUserId = req.user._id;
+        const fromUserId = req.params.fromUserId;
+        const status = req.params.status;
+
+        const allowedStatus = ["accepted", "rejected"].includes(status);
+        if (!allowedStatus) {
+            throw new Error("Invalid Status", status);
+        }
+
+        const reviewRequest = await ConnectionRequest.findOne({
+            fromUserId: fromUserId,
+            toUserId: toUserId,
+            status: status,
+        })
+
+        if (!reviewRequest) {
+            throw new Error("No Request Request Found!");
+        }
+
+        reviewRequest.status = status;
+        await reviewRequest.save();
+
+        res.status(200).json({ message: `${req.user.firstName}, You have ${status} the Request!` })
+
+    } catch (error) {
+        res.status(500).json({ message: "Error", error: error.message });
+    }
+});
+
+connectionRouter.post('')
 
 
 
