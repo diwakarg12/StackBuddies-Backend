0a5bd627d9d41ed27dc7d008514063fc91cbf3f4
Diwakar Giri <diwakargiri23@gmail.com>
2025-02-27 11:16:21 +0530

    created login, signup, logout and profile API

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,8 +1,71 @@
 const express = require('express');
 const userAuth = require('../middlewares/userAuth.middleware');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
+const { signupValidation, loginValidation } = require('../Utils/apiValidation');
 
 const authRouter = express.Router();
+
+authRouter.post('/signup', async (req, res) => {
+    try {
+        signupValidation(req.body);
+        const { firstName, lastName, email, password } = req.body;
+
+        const existedUser = await User.findOne({ email: email });
+        if (existedUser) {
+            throw new Error("User Already Existed");
+        }
+
+        const passwordHash = await bcrypt.hash(password, 10)
+
+        const user = new User({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: passwordHash
+        });
+
+        await user.save();
+        res.status(200).json({ message: "User Created SuccessFully", user: user })
+    } catch (error) {
+        res.status(200).json({ message: "Error", error: error.message });
+    }
+
+})
+
+authRouter.post('/login', async (req, res) => {
+    try {
+        loginValidation(req.body)
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            throw new Error("Invalid Credential");
+        }
+
+        const pass = await bcrypt.compare(password, user.password);
+        if (!pass) {
+            throw new Error("Invalid Credential");
+        }
+
+        const token = jwt.sign({ _id: user._id }, "Diwakar@123", { expiresIn: '1d' })
+        console.log('Token', token)
+        if (!token) {
+            throw new Error("Error while Generating Token");
+        }
+
+        res.cookie('token', token);
+        res.status(200).json({ message: "Login Successfull" })
+
+    } catch (error) {
+        res.status(500).json({ message: "Error", error: error.message });
+    }
+})
+
+authRouter.post('/logout', async (req, res) => {
+    const token = res.cookie('token', null, { expires: new Date(Date.now()) });
+    res.status(200).json({ message: "user LoggedOut Successfully" })
+})
 
 module.exports = authRouter;
--- src/routers/profileRouter.js
+++ src/routers/profileRouter.js
@@ -4,5 +4,23 @@ const User = require('../models/user.model');
 
 const profileRouter = express.Router();
 
+profileRouter.get('/view', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        if (!loggedInUser) {
+            throw new Error("Unable to Get user Data");
+        }
+
+        res.status(200).json({ message: "User Data Fetched", user: loggedInUser })
+    } catch (error) {
+        res.status(500).json({ message: "Error", error: error.message });
+    }
+});
+
+profileRouter.post('/edit', userAuth, async(req, res) => {
+     
+})
+
 
 module.exports = profileRouter;
